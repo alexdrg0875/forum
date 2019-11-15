05733f3e54eaf4a90752920da0295b8f9425b2eb
Deryhlazov Oleksandr <oleksandr.deryhlazov75@gmail.com>
2019-11-15 14:39:36 +0300

    complete 3 task with test a_thread_can_have_replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -18,5 +18,27 @@
                 </div>
             </div>
         </div>
+
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach($thread->replies as $reply)
+                    <div class="card">
+                        <div class="card-header">
+                            <a href="#">
+                                {{ $reply->owner->name }}
+                            </a> said {{ $reply->created_at->diffForHumans() }}
+                        </div>
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            {{ $reply->body }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
