a2fd34565cb10c38589d4169809e6d462aa3a5f9
Deryhlazov Oleksandr <oleksandr.deryhlazov75@gmail.com>
2019-11-15 19:47:15 +0300

    coplete 5th task with Reply Form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,9 +6,9 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                    <a href="#">
-                        {{ $thread->creator->name }}
-                    </a> posted:
+                        <a href="#">
+                            {{ $thread->creator->name }}
+                        </a> posted:
                         {{ $thread->title }}
                     </div>
 
@@ -31,5 +31,22 @@
                 @endforeach
             </div>
         </div>
+
+        @if(auth()->check())
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        @csrf
+                        <div class="form-group">
+                           <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
+                </div>
+            </div>
+            @else
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in </a> to participate in this discussion.</p>
+        @endif
     </div>
 @endsection
