0aa3dbdf4b28812cc727f43bd6e62e0769b722e5
Deryhlazov Oleksandr <oleksandr.deryhlazov75@gmail.com>
2019-11-22 22:52:36 +0300

    complete task-20 From-56-Queries-Down-to-Two

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,19 +3,13 @@
 namespace App\Providers;
 
 use App\Channel;
+
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+
 
     /**
      * Bootstrap any application services.
@@ -24,9 +18,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \View::composer('*', function ($view){
-            $view->with('channels', Channel::all());
+        \View::composer('*', function ($view) {
+            $channels = Cache::rememberForever('channels', function () {
+                return Channel::all();
+            });
+            $view->with('channels', $channels);
         });
 //        \View::share('channels', Channel::all());
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+//        if ($this->isLocal()) {
+//            $this->register(\Barryvdh\Debagbar\ServiceProvider::class);
+//        }
     }
 }
