7847134b5512e0f1eb79b7ad4a08d3d9edbbfcc8
Deryhlazov Oleksandr <oleksandr.deryhlazov75@gmail.com>
2019-12-02 21:40:23 +0300

    complete task-36 Extracting-Components-With-Blade-Functionality

---
+++ resources/js/pages/Thread.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+    import Replies from '../components/Replies.vue';
+
+    export default {
+        name: "Thread",
+
+        props:['initialRepliesCount'],
+
+        components: {Replies},
+
+        data() {
+            return {
+                repliesCount: this.initialRepliesCount
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,73 +1,73 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="level">
+    <thread-view :initial-replies-count="{{ $thread->replies_count }}" inline-template>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="level">
                             <span class="flex">
                         <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
                                 {{ $thread->title }}
                             </span>
 
-                            @can('update', $thread)
-                                <form action="{{ $thread->path() }}" method="POST">
-                                    @csrf
-                                    {{ method_field('DELETE') }}
-                                    <button type="submit" class="btn btn-link">Delete Thread</button>
-                                </form>
-                            @endcan
+                                @can('update', $thread)
+                                    <form action="{{ $thread->path() }}" method="POST">
+                                        @csrf
+                                        {{ method_field('DELETE') }}
+                                        <button type="submit" class="btn btn-link">Delete Thread</button>
+                                    </form>
+                                @endcan
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            {{ $thread->body }}
                         </div>
                     </div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        {{ $thread->body }}
-                    </div>
-                </div>
-                <br>
-                @foreach($replies as $reply)
-                    @include('threads.reply')
                     <br>
-                @endforeach
 
-                {{ $replies->links() }}
+                    <replies :data="{{ $thread->replies }}" @removed="repliesCount--"></replies>
 
-                @if(auth()->check())
-                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
-                        @csrf
-                        <div class="form-group">
+                    {{--{{ $replies->links() }}--}}
+
+                    @if(auth()->check())
+                        <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                            @csrf
+                            <div class="form-group">
                             <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"
                                       rows="5"></textarea>
-                        </div>
+                            </div>
 
-                        <button type="submit" class="btn btn-outline-secondary">Post</button>
-                    </form>
-                @else
-                    <p class="text-center">Please <a href="{{ route('login') }}">sign in </a> to participate in this
-                        discussion.
-                    </p>
-                @endif
-            </div>
-
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">
-                        <p>
-                            This thread was published {{ $thread->created_at->diffForHumans() }} by
-                            <a href="#">{{ $thread->creator->name }}</a>, and corrently
-                            has {{ $thread->replies_count }}  {{ str_plural('comment', $thread->replies_count) }}.
+                            <button type="submit" class="btn btn-outline-secondary">Post</button>
+                        </form>
+                    @else
+                        <p class="text-center">Please <a href="{{ route('login') }}">sign in </a> to participate in this
+                            discussion.
                         </p>
+                    @endif
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <p>
+                                This thread was published {{ $thread->created_at->diffForHumans() }} by
+                                <a href="#">{{ $thread->creator->name }}</a>, and corrently
+                                has <span v-text="repliesCount"></span>  {{ str_plural('comment', $thread->replies_count) }}.
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
         </div>
-    </div>
+    </thread-view>
 @endsection
