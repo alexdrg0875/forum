f73725a14454f78c6c815d5b491446d90d57f05d
Deryhlazov Oleksandr <oleksandr.deryhlazov75@gmail.com>
2019-12-03 16:44:38 +0300

    complete task-37 A-New-Reply-Vue-Component

--- app/Thread.php
+++ app/Thread.php
@@ -26,31 +26,52 @@ class Thread extends Model
     }
 
 
+    /**
+     * @return string
+     */
     public function path()
     {
         return "/threads/{$this->channel->slug}/{$this->id}";
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function replies()
     {
         return $this->hasMany(Reply::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function creator()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    /**
+     * @param $reply
+     * @return Model
+     */
     public function addReply($reply)
     {
-        $this->replies()->create($reply);
+        return $this->replies()->create($reply);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function channel()
     {
         return $this->belongsTo(Channel::class);
     }
 
+    /**
+     * @param $query
+     * @param $filters
+     * @return mixed
+     */
     public function scopeFilter($query, $filters)
     {
         return $filters->apply($query);
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -34,25 +34,10 @@
                     </div>
                     <br>
 
-                    <replies :data="{{ $thread->replies }}" @removed="repliesCount--"></replies>
-
+                    <replies :data="{{ $thread->replies }}"
+                             @added="repliesCount++"
+                             @removed="repliesCount--"></replies>
                     {{--{{ $replies->links() }}--}}
-
-                    @if(auth()->check())
-                        <form method="POST" action="{{ $thread->path() . '/replies' }}">
-                            @csrf
-                            <div class="form-group">
-                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"
-                                      rows="5"></textarea>
-                            </div>
-
-                            <button type="submit" class="btn btn-outline-secondary">Post</button>
-                        </form>
-                    @else
-                        <p class="text-center">Please <a href="{{ route('login') }}">sign in </a> to participate in this
-                            discussion.
-                        </p>
-                    @endif
                 </div>
 
                 <div class="col-md-4">
