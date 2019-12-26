86a4e669dd07ac18e4d8a0da674f56e647d7c7ee
Deryhlazov Oleksandr <oleksandr.deryhlazov75@gmail.com>
2019-12-26 18:10:20 +0300

    completed task-71 Users-Must-Confirm-Their-Email-Address

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -59,7 +59,6 @@ class ThreadsController extends Controller
      */
     public function store(Request $request)
     {
-
         $request->validate([
             'title' => 'required|spamfree',
             'body' => 'required|spamfree',
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'confirmed' => false,
     ];
 });
 
@@ -48,7 +49,7 @@ $factory->define(Thread::class, function ($faker){
         },
         'title' => $faker->sentence,
         'body' => $faker->paragraph,
-        'visits' => 0
+        'visits' => 0,
     ];
 });
 
