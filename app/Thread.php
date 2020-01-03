8f712ea6431bc14022c918033b5a1c048006c3cf
Deryhlazov Oleksandr <oleksandr.deryhlazov75@gmail.com>
2020-01-03 23:33:32 +0300

    completed task-94 First-Class-Search-Part-1-A-Review

--- app/Thread.php
+++ app/Thread.php
@@ -4,13 +4,13 @@ namespace App;
 
 use App\Events\ThreadHasNewReply;
 use App\Events\ThreadReceivedNewReply;
-use App\Notifications\ThreadWasUpdated;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Laravel\Scout\Searchable;
 
 class Thread extends Model
 {
-    use RecordsActivity;
+    use RecordsActivity, Searchable;
 
     protected $guarded = [];
 
